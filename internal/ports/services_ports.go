f3feff65324b9627702b352270b340ffaf4e0f9d
FredySosa <fredsar751@gmail.com>
2022-02-15 17:42:41 -0600

    Improve folders organization (#1)
    Signed-off-by: Fredy Sosa <fredsar751@gmail.com>

--- internal/ports/services_ports.go
+++ internal/ports/services_ports.go
@@ -6,10 +6,6 @@ import (
 	"github.com/FredySosa/hexArch/internal/core/domain"
 )
 
-type EventService interface {
-	GetCountries(ctx context.Context, limit, offset string) ([]domain.Country, string, error)
-}
-
 type CountriesService interface {
 	GetCountries(ctx context.Context, data domain.QueryData) ([]domain.Country, string, error)
 }
